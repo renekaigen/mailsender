182bc906c94073491f104fea7e8c636d7fd52d15
Rene Soto <rene.soto01@iest.edu.mx>
2015-04-09 12:10:45 -0500

    Proyecto Android para enviar mail con archivo adjunto sin uso de intents para automatizar procesos.

---
+++ app/src/main/java/com/example/root/sendmail/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.root.sendmail;
+
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        //  http://www.jondev.net/articles/Sending_Emails_without_User_Intervention_(no_Intents)_in_Android
+
+        Button addImage = (Button) findViewById(R.id.send_email);
+        addImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Mail m = new Mail("autoresponder.reply@gmail.com", "autoresponder123");
+
+                String[] toArr = {"rene.soto01@iest.edu.mx","rene.soto@krow.mx","carlos.galarza@iest.edu.mx"};
+                m.setTo(toArr);
+                m.setFrom("autoresponder.reply@gmail.com");
+                m.setSubject("This is an email sent using my Mail JavaMail wrapper from an Android device.");
+                m.setBody("Email body.");
+
+                try {
+
+                    /*
+                     m.addAttachment("/sdcard/filelocation");
+
+                            Enviando mi archivo de sd
+                     */
+                    File sdCard = Environment.getExternalStorageDirectory();
+                    File dir = new File (sdCard.getAbsolutePath() + "/myLogcat");
+                    dir.mkdirs();
+                    m.addAttachment(dir+"/logcat.txt");
+
+                    if(m.send()) {
+                        Toast.makeText(MainActivity.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Email was not sent.", Toast.LENGTH_LONG).show();
+                    }
+                } catch(Exception e) {
+                    //Toast.makeText(MailApp.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+                    Log.e("MailApp", "Could not send email", e);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
